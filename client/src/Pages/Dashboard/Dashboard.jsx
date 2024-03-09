8e46d94976c198e127414fa2c3479fc66adf5d3f
Miraz PC <abdullahmiraz2015@gmail.com>
2024-03-09 08:51:55 +0600

    uploaded the total ui, now update the react pdf feature

--- client/src/Pages/Dashboard/Dashboard.jsx
+++ client/src/Pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Dashboard = () => {
   const [title, setTitle] = useState("");
   const [file, setFile] = useState("");
+  const [allImage, setallImage] = useState(null);
+
+  useEffect(() => {
+    getPdf();
+  }, []);
+
+  const getPdf = async () => {
+    const result = await axios.get(`http://localhost:5000/get-files`);
+    console.log(result.data.data);
+    setallImage(result.data.data);
+  };
 
   const submitImage = async (e) => {
     e.preventDefault();
@@ -10,13 +22,36 @@ const Dashboard = () => {
     formData.append("title", title);
     formData.append("file", file);
     console.log(title, file);
+
+    try {
+      const result = await axios.post(
+        "http://localhost:5000/upload-files",
+        formData,
+        {
+          headers: {
+            "Content-type": "multipart/form-data",
+          },
+        }
+      );
+      console.log(result);
+      if (result.data.status == "ok") {
+        alert("Uploaded Successfully!!!");
+        getPdf();
+      }
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    }
+  };
+
+  const showPdf = (pdf) => {
+    window.open(`http://localhost:5000/files/${pdf}`, "_blank", "norefferer");
   };
 
   return (
     <div>
       <h1>Admin dashboard</h1>
-      <div className="input-files w-[40vw] border mx-auto m-4 p-4">
-        <form action="" className="formStyle">
+      <div className="input-files w-[80vw] border mx-auto m-4 p-4">
+        <form onSubmit={submitImage} className="formStyle">
           <h3>Upload, edit, delete pdf</h3> <br />
           <div className="uploadfile">
             <input
@@ -29,7 +64,7 @@ const Dashboard = () => {
             <br />
             <input
               type="file"
-              className=" form-control  "
+              className="form-control"
               accept="application/pdf"
               required
               onChange={(e) => setFile(e.target.files[0])}
@@ -40,6 +75,30 @@ const Dashboard = () => {
             Submit
           </button>
         </form>
+
+        <div className="divider"></div>
+
+        {/* for showing the pdf that are on the database */}
+        <div className="uploaded flex flex-col justify-center items-center text-center font-bold">
+          <h4>Uploaded PDF: </h4>
+          <div className="output-div">
+            {allImage == null
+              ? ""
+              : allImage.map((data) => {
+                  return (
+                    <div id={data._id} className="inner-div">
+                      <h6>Title: </h6>
+                      <button
+                        onClick={() => showPdf(data.pdf)}
+                        className="btn btn-sm btn-primary"
+                      >
+                        Show PDF
+                      </button>
+                    </div>
+                  );
+                })}
+          </div>
+        </div>
       </div>
     </div>
   );
